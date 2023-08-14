7ad8bfd8274b389125db42999e0572cb73cc0f6c
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2023-08-14 19:18:35 +0530

    Add files via upload

---
+++ ECommerce/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<title>E Commerce Website</title>
+  	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,100;1,300&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+  </head>
+  <body>
+  	
+  <?php include 'menu.php';?>
+
+  <div class="jumbotron">
+  <h1>Neha Sharma</h1>
+  <p>Thanks for your valuable time :)</p>
+</div>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row" >
+
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/img3.png" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">I am Neha Sharma</h2>
+      <p class="py-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <a href="about.php" class="btn btn-success">Now More About me</a>
+    </div>
+    
+  </div>
+  </div>
+  
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@Neha Sharma</p>
+</footer>
+
+  </body>
+  </html>
---
+++ ECommerce/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>E Commerce Website</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,100;1,300&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	
+
+
+<?php include 'menu.php';?>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/home1.png" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/home2.png" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/home3.png" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">On Sale Products</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row" >
+
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="images/sec.png" class="img-fluid aboutimg">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2 class="display-4">I am Neha Sharma</h2>
+			<p class="py-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<a href="about.php" class="btn btn-success">Now More About me</a>
+		</div>
+		
+	</div>
+	</div>
+	
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Services</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+					<img class="card-img-top" src="images/img2.png" alt="Card image">
+					<div class="card-body">
+						<h4 class="card-title">Beautiful Nature</h4>
+						<p class="card-text">Some example text.</p>
+						<a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+					<img class="card-img-top" src="images/img2.png" alt="Card image">
+					<div class="card-body">
+						<h4 class="card-title">Beautiful Nature</h4>
+						<p class="card-text">Some example text.</p>
+						<a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" >
+					<img class="card-img-top" src="images/img2.png" alt="Card image">
+					<div class="card-body">
+						<h4 class="card-title">Beautiful Nature</h4>
+						<p class="card-text">Some example text.</p>
+						<a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</section>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Gallery</h2>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="images/img1.png" class="img-fluid pb-4 ml-3">
+		</div>
+
+	</div>
+</section>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Connect With Us!</h2>
+	</div>
+
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>UserName</label>
+				<input type=
+				"text" name="user" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email Id</label>
+				<input type=
+				"text" name="email" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Mobile</label>
+				<input type=
+				"text" name="mobile" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Comment</label>
+				<textarea class="form-control" name="comment">
+				</textarea>
+			</div>
+			<button type="submit" class="btn btn-success">Submit</button>
+		</form>
+	</div>
+</section>
+
+<footer>
+	<p class="p-3 bg-dark text-white text-center">@Neha Sharma</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+ </body>
+</html>
